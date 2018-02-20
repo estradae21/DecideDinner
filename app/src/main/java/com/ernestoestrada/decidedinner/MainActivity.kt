1018f5a54baa0ba8a1d616854cd9285281b73cf7
Ernesto Estrada <eestrad9@msudenver.edu>
2018-02-20 16:51:48 -0700

    App is functional

--- app/src/main/java/com/ernestoestrada/decidedinner/MainActivity.kt
+++ app/src/main/java/com/ernestoestrada/decidedinner/MainActivity.kt
@@ -3,36 +3,40 @@ package com.ernestoestrada.decidedinner
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.selector
 import java.io.OutputStreamWriter
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val foodList = arrayListOf<CharSequence>("Sushi", "Chinese")
+    private val FNAME = "dataStorage.txt"
+    val foodList = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getFoodList()
 
         var actionBar = getSupportActionBar()
         actionBar!!.setDisplayShowTitleEnabled(false)
 
         decideBtn.setOnClickListener {
-            val random = Random()
-            val randomFood = random.nextInt(foodList.count())
-            SelectedFood.text = foodList[randomFood]
+            if (foodList.isNotEmpty()) {
+                decideBtn()
+            }
+            else
+                Toast.makeText(applicationContext, "You have no food items",
+                        Toast.LENGTH_SHORT).show()
         }
 
         addFoodBtn.setOnClickListener {
             val newFood = addFoodTxt.text.toString()
             if (newFood.trim().length > 0){
                 foodList.add(newFood)
-                addFood(newFood)
                 addFoodTxt.text.clear()
                 Toast.makeText(applicationContext, "Your food item was saved",
                         Toast.LENGTH_SHORT).show()
@@ -44,12 +48,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun decideBtn(){
+        val random = Random()
+        val randomFood = random.nextInt(foodList.count())
+        SelectedFood.text = foodList[randomFood]
+    }
+
     fun getFoodList(){
         try {
-            val f = openFileInput("dataStorage.txt")
+            val f = openFileInput(FNAME)
             val br = f.bufferedReader()
             br.forEachLine {
                 foodList.add(it)
+                println(it)
             }
             f.close()
         }
@@ -57,18 +68,35 @@ class MainActivity : AppCompatActivity() {
                 print(ex.message)}
         }
 
-    fun addFood(str: String) {
+    fun saveList() {
         try {
-            val f = openFileOutput("dataStorage.txt", Context.MODE_APPEND)
+            val f = openFileOutput(FNAME, Context.MODE_PRIVATE)
             val Fout = OutputStreamWriter(f)
-            Fout.append(str + "\n")
+            for (element in foodList){
+                Fout.append("$element\n")
+                println(element)
+            }
+
             Fout.close()
-            println(foodList)
         } catch (ex: Exception) {
             print(ex.message)
         }
     }
 
+    fun deleteItem(){
+        //getFoodList()
+        selector("Which one to delete?" , foodList, {dialogInterface, i ->
+            foodList.removeAt(i)
+            Toast.makeText(applicationContext, "It has been removed",
+                Toast.LENGTH_SHORT).show()})
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_settings -> true
@@ -78,9 +106,24 @@ class MainActivity : AppCompatActivity() {
                         Toast.LENGTH_SHORT).show()
                 return true
             }
+            R.id.action_delete -> {
+                deleteItem()
+                return true
+            }
 
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        foodList.clear()
+        getFoodList()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveList()
     }
 }
 
